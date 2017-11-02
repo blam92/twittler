a66e91d9900e4d22e850461bbd474855a601530e
Santi Iraola <santi@Santis-MacBook-Pro.local>
2017-11-02 13:43:23 -0300

    Added cardviews and displays filtered twits by user when clicking on username.

--- display.js
+++ display.js
@@ -30,24 +30,29 @@ function getImg() {
 }
 
 function loadTweets(tweets, elementAfterTwitsAreInserted, numberOfTwitsToLoad) {
-  var index = streams.home.length - 1;
+  var index = tweets.length - 1;
   while(index >= (tweets.length - numberOfTwitsToLoad)) {
     var tweet = tweets[index];
-
-    var $tweet = $('<div class="scrollme animateme" data-when="enter" data-from="0.5" data-to="0" data-crop="false" data-opacity="0" data-scale="1.5" style="opacity: 1; transform: translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale3d(1, 1, 1);"><div class="twit">\
+    var twitColor = colorSelector.getColor();
+    var $tweet = $('<div class="scrollme animateme" data-when="enter" data-from="0.5" data-to="0" data-crop="false" data-opacity="0" data-scale="1.5" style="opacity: 1; transform: translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale3d(1, 1, 1);">\
+      <div class="twit" style="background-color:' + twitColor + '">\
       <p class="twit-date">' + tweet.created_at +'</p>\
       <div class="horizontal">\
         <img src="' + getImg() + '" class="profile_icon"></img>\
-        <h1 class="twit-user">@' + tweet.user +'</h1>\
+        <a class="twit-user">@' + tweet.user +'</a>\
       </div>\
       <p class="twit-content">' + tweet.message +'</p>\
     </div></div>');
-
-    twitColor = colorSelector.getColor();
-    $tweet.css('background-color', twitColor);
     $tweet.insertBefore(elementAfterTwitsAreInserted);
     index -= 1;
   }
+}
+
+function filterByUser(tweets, user) {
+  //user should be without @
+  return tweets.filter(function(item) {
+    return item.user === user;
+  });
 }
 
 $(document).ready(function() {
@@ -58,7 +63,18 @@ $(document).ready(function() {
     var lastTweet = $('.scrollme').first()
     loadTweets(streams.home, lastTweet, 10);
   });
-});
 
+  $('.twit-user').on('click', function() {
+    $('body').children('div').html('');
+    var selectedUserTweets = filterByUser(streams.home, $(this).text().slice(1));
+    loadTweets(selectedUserTweets, $loadMoreButton, selectedUserTweets.length);
+  });
+
+  $('.title').on('click', function() {
+    $('body').children('div').html('');
+    loadTweets(streams.home, $loadMoreButton, streams.home.length);
+  });
+
+});
 
 

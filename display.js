1e0e5e1e43c4419a8f8ba26b0e582549746d2ea6
Santi Iraola <santi@Santis-MacBook-Pro.local>
2017-11-02 08:52:25 -0300

    added scrollme animation.

--- display.js
+++ display.js
@@ -28,25 +28,27 @@ function getImg() {
 }
 
 $(document).ready(function(){
-  var $body = $('body');
-  //$body.html('');
-
+  var $loadMoreButton = $('.load_twits');
   var index = streams.home.length - 1;
   while(index >= 0) {
     var tweet = streams.home[index];
-    var $tweet = $('<div class="twit">\
+
+    var $tweet = $('<div class="scrollme animateme" data-when="enter" data-from="0.5" data-to="0" data-crop="false" data-opacity="0" data-scale="1.5" style="opacity: 1; transform: translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale3d(1, 1, 1);"><div class="twit">\
       <p class="twit-date">' + tweet.created_at +'</p>\
       <div class="horizontal">\
         <img src="' + getImg() + '" class="profile_icon"></img>\
         <h1 class="twit-user">@' + tweet.user +'</h1>\
       </div>\
       <p class="twit-content">' + tweet.message +'</p>\
-    </div>');
+    </div></div>');
+
     twitColor = getColor(colorCodes, lastColor);
     $tweet.css('background-color', twitColor);
     lastColor = twitColor;
-    $tweet.appendTo($body);
-    console.log(lastColor);
+    $tweet.insertBefore($loadMoreButton);
     index -= 1;
-    }
+  }
 });
+
+
+
